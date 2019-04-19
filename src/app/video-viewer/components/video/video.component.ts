fa2aa8a8e952b10ee326ef4298ab1668c61e02e2
Edwino Stein <edwino.stein@gmail.com>
2019-04-18 20:03:32 -0400

    Adicionado VideoComponent [Issue #6]

---
+++ src/app/video-viewer/components/video/video.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, ElementRef, Renderer, HostListener, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-video',
+  templateUrl: './video.component.html',
+  styleUrls: ['./video.component.css']
+})
+export class VideoComponent implements OnInit {
+
+    @Input() aspectRatio: string = "4:3";
+    private _aspectRario: number[];
+
+    screen: any;
+    height: number;
+
+    @HostListener('window:resize', ['$event'])
+    onResize(event) {
+        this.updateSize();
+    }
+
+    constructor(
+        private elementRef: ElementRef,
+        private renderer: Renderer
+    ) {}
+
+    ngOnInit() {
+
+        let ar = this.aspectRatio.split(':');
+        this._aspectRario = [Number(ar[0]), Number(ar[1])];
+
+        this.screen = this.elementRef.nativeElement.getElementsByClassName("screen")[0];
+        this.updateSize();
+    }
+
+    updateSize(){
+
+        let maxHeight: number = window.document.body.offsetHeight - (20 + 50 + 20);
+        let maxWidth: number = maxHeight * (this._aspectRario[0]/this._aspectRario[1]);
+        this.renderer.setElementStyle(this.screen, "max-width", maxWidth.toString() + "px");
+
+        let hight: number = this.screen.offsetWidth * (this._aspectRario[1]/this._aspectRario[0]);
+        this.renderer.setElementStyle(this.screen, "height", hight.toString() + "px");
+
+    }
+}
