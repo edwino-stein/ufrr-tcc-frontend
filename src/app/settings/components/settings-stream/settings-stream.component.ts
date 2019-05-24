b1e96bcd37ff5e1ce69addc82dce913ed67bc08e
Edwino Stein <edwino.stein@gmail.com>
2019-05-24 19:14:44 -0400

    Implementando funcionabilidade start/stop para a transmissão [Issue #10]

--- src/app/settings/components/settings-stream/settings-stream.component.ts
+++ src/app/settings/components/settings-stream/settings-stream.component.ts
@@ -1,14 +1,62 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { StreamService } from '../../../shared/services/stream.service'
 
 @Component({
     selector: 'app-settings-stream',
     templateUrl: './settings-stream.component.html',
     styleUrls: ['./settings-stream.component.css']
 })
-export class SettingsStreamComponent implements OnInit {
+export class SettingsStreamComponent implements OnInit, OnDestroy {
 
-    constructor() { }
+    private statusChangedSubs: Subscription;
+    private actionResponseSubs: Subscription;
 
-    ngOnInit() {}
+    loading: boolean = true;
+    status: String = 'NONE';
+    watchingPeaple: String = 'XX';
 
+    constructor(private streamService: StreamService){}
+
+    ngOnInit(){
+
+        if(this.streamService.getCurrentStatus() != 'NONE'){
+            this.loading = false;
+            this.status = this.streamService.getCurrentStatus();
+        }
+
+        this.statusChangedSubs = this.streamService.statusChanged.subscribe(
+            (status) => this.onStatusChange(status)
+        );
+
+        this.actionResponseSubs = this.streamService.actionResponse.subscribe(
+            (respose) => this.onAction(respose)
+        );
+    }
+
+    ngOnDestroy(){
+        this.statusChangedSubs.unsubscribe();
+        this.actionResponseSubs.unsubscribe();
+    }
+
+    private onStatusChange(status: any): void {
+        this.loading = false;
+        this.status = status['status'];
+        console.log(status['status']);
+    }
+
+    private onAction(respose: any): void {
+        this.loading = false;
+        this.status = respose.respose.data.status;
+    }
+
+    onStartBtnClicked(){
+        this.loading = true;
+        this.streamService.start();
+    }
+
+    onStopBtnClicked(){
+        this.loading = true;
+        this.streamService.stop();
+    }
 }
