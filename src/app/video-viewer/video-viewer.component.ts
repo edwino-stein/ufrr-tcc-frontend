b31f21c1490a98012c0fe9462feb24227b4830f1
Edwino Stein <edwino.stein@gmail.com>
2019-05-27 20:52:01 -0400

    Adicionando suporte a feedback na barra de status da transmissão [Closes #14]

--- src/app/video-viewer/video-viewer.component.ts
+++ src/app/video-viewer/video-viewer.component.ts
@@ -1,14 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+
+import { StreamService } from '../shared/services/stream.service';
 
 @Component({
     selector: 'app-video-viewer',
     templateUrl: './video-viewer.component.html',
     styleUrls: ['./video-viewer.component.css']
 })
-export class VideoViewerComponent implements OnInit {
+export class VideoViewerComponent implements OnInit, OnDestroy {
 
-    constructor() { }
+    private statusChangedSubs: Subscription;
 
-    ngOnInit() { }
+    loading: boolean = true;
+    status: String = 'WAITTING';
+    watchingPeaple: number = 0;
 
+    constructor(private streamService: StreamService) { }
+
+    ngOnInit(){
+
+        if(this.streamService.getCurrentStatus() != 'WAITING'){
+            this.loading = false;
+            this.status = this.streamService.getCurrentStatus();
+        }
+
+        this.statusChangedSubs = this.streamService.statusChanged.subscribe(
+            (status) => this.onStatusChange(status)
+        );
+    }
+
+    ngOnDestroy(){
+        this.statusChangedSubs.unsubscribe();
+    }
+
+    private onStatusChange(status: any): void {
+        this.loading = false;
+        this.status = status['status'];
+        console.log(status['status']);
+    }
 }
