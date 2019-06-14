f15ac658bdf57e5c072ca56f4e312012c5868b0b
Edwino Stein <edwino.stein@gmail.com>
2019-06-14 19:22:05 -0400

    Adicionada tela inicial [Closes #18]

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Router } from "@angular/router"
+import { Subscription } from 'rxjs';
+
+import { AuthService, LoginResponseData } from '../../shared/services/auth.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+    private loading: boolean = false;
 
-  ngOnInit() {
-  }
+    private LoginSubs: Subscription;
+    private LogoutSubs: Subscription;
 
+    constructor(
+        private auth: AuthService,
+        private router: Router
+    ){}
+
+    ngOnInit(){
+
+        this.LoginSubs = this.auth.onLogin.subscribe(
+            (response) => this.onLoginResponde(response)
+        );
+
+        this.LogoutSubs = this.auth.onLogout.subscribe(
+            (response) => this.onLogoutResponde(response)
+        );
+    }
+
+    ngOnDestroy(){
+        this.LoginSubs.unsubscribe();
+        this.LogoutSubs.unsubscribe();
+    }
+
+    onLoginResponde(response: LoginResponseData): void {
+        this.loading = false;
+        if(response.success){
+            this.router.navigate(['/settings']);
+        }
+    }
+
+    onLogout(){
+        this.auth.doLogout();
+    }
+
+    onLogoutResponde(response: LoginResponseData): void {
+        this.loading = false;
+    }
 }
